787d1be6b4b4c4c4c8987c02294efe8f45e80ae0
James Rogers <jrogers.dev@gmail.com>
2020-11-13 01:29:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ document.addEventListener("keydown", function(e) {
   } 
   else if (e.key === "ArrowRight") {
     let leftNumbers = dodger.style.left.replace("px", "");
-    
+    let rightNumbers = dodger.style.right.replace("px", "");
     let left = parseInt(leftNumbers, 10);
+    let right = parseInt(rightNumbers, 10);
     
-    if (left + dodger.style.left = `${left + 2}px`;
+    if (right < 400) dodger.style.left = `${left + 2}px`;
   }
 });
 
